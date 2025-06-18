ce10b425bbeaafc9d7bcb88079711dddc03c1a6f
VipulG23 <vipulgajbhiye78@gmail.com>
2025-06-19 00:24:48 +0530

    chore: update CORS for Vercel frontend

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import path from "path";
 dotenv.config();
 
 const PORT = process.env.PORT;
-
+const __dirname = path.resolve();
 
 
 
@@ -21,14 +21,23 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://chat-app-frontend-fwvh-e0trh9zue.vercel.app",
+    ],
+
     credentials: true,
   })
 );
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+  });
+}
  
 
 
